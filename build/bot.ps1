b3c9d21f4d101834fe0a393d01fa49f91b994c60
stan e <yi.hang@live.com>
2024-05-07 15:35:13 +0800

    测试

--- build/bot.ps1
+++ build/bot.ps1
@@ -8,10 +8,8 @@ $currentDate = (Get-Date)
 $currentDateStr = $currentDate.ToString('yyyy-MM-dd HH:mm:ss')
 
 $rootPath = Split-Path -Parent (Get-Location).Path
+
 # 读取当前项目配置
-
-
-
 $ciConfigPath = Join-Path $rootPath "src" "ci-config.json"
 $ciConfig = (Get-Content -Path $ciConfigPath -Encoding UTF8) | ConvertFrom-Json
 
@@ -22,6 +20,11 @@ if ($ciConfig.mode -eq 'tag') {
 $branchOrTag = $ciConfig.branch
 $commit = $ciConfig.commit
 
+# 环境变量参数
+$repositoryUrl = $env:repositoryUrl
+$run_id = $env:run_id
+$run_number = $env:run_number
+
 $noticeMsg = @"
 ----------- ${currentDateStr} -----------
 ${Msg}
@@ -29,6 +32,9 @@ ${Msg}
 ----------- build info -----------
 ${branchOrTagKey}: ${branchOrTag}
 commit: ${commit}
+repositoryUrl: ${repositoryUrl}
+run_id: ${repositoryUrl}
+run_number: ${repositoryUrl}
 "@
 
 
